eabf8f7ddf1fb9f2717684a8317b6ae88390d12a
Artur Taranchiev <github@in.ewok.email>
2025-12-12 22:44:56 +0600

    refactor(currency): format code
    - Ensured consistent formatting in Currency methods
    
    goal: improve code maintainability

--- internal/exchange/currency.go
+++ internal/exchange/currency.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-     http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,21 +18,21 @@ package exchange
 import "strings"
 
 type Currency struct {
-    Code string
+	Code string
 }
 
 func (c Currency) String() string {
-    return string(c.Code)
+	return string(c.Code)
 }
 
 func (c Currency) GetLCode() string {
-    return strings.ToLower(string(c.Code))
+	return strings.ToLower(c.Code)
 }
 
 func (c Currency) GetCode() string {
-    return strings.ToUpper(string(c.Code))
+	return strings.ToUpper(c.Code)
 }
 
 func NewCurrency(code string) Currency {
-    return Currency{Code: strings.ToUpper(code)}
+	return Currency{Code: strings.ToUpper(code)}
 }
